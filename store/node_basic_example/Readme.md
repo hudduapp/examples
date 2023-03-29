25bf2ad8f5eb6673b3255b9a905daf5dbad70379
Joshua3212 <joshua-koeck@gmx.de>
2023-03-29 18:48:22 +0200

    store examples for nodejs and python

---
+++ store/node_basic_example/Readme.md
@@ -0,0 +1,8 @@
+# Node Basic Example
+
+### Description
+
+A dead simple example of how to use huddu store with NodeJS.
+
+Make sure to set `client_secret` and `client_id` to your store's values.
+You can create a new store at https://huddu.io/app/store
--- store/python_basic_example/index.py
+++ store/python_basic_example/index.py
@@ -2,42 +2,41 @@ from math import acos
 
 from huddu import Store
 
-###
-# Create a new store: https://huddu.io/login?next=/integrations/store
-# In this example we will calculate pi; store it and then access it again to calculate the area of a circle.
-###
+"""
+    Create a new store on the dashboard at https://huddu.io/login?next=/app/store
+    In this example we will calculate pi; store it + a radius r and then access it again to calculate the area of a circle.
+"""
 
-
-store = Store("<paste_your_store_token_here>")  # Check https://huddu.io/docs/Store-SDK for more information
+store = Store(
+    client_id="<your_client_id>",
+    client_secret="<your_client_secret>"
+)  # Check https://huddu.io/docs/Store-SDK for more information
 
 
 def calc_pi(decimal_places):
     return round(2 * acos(0.0), decimal_places)
 
 
+# Store pi and r in a store
 store.put("pi", calc_pi(3))
-# let's store more decimal places...
-store.update("pi", calc_pi(5))
-
-# let's store an example radius r
 store.put("r", 100)
 
+# update key "pi"
+store.update("pi", calc_pi(5))
 
-###
-# Imagine now accessing this data on a different machine
-###
 
+# on a different device with internet access...
 def calc_circle(pi, r):
     return pi * r ** 2
 
 
+# Retrieve our values using the keys we used before
 pi = store.get("pi")
 r = store.get("r")
 
 print("the area of the circle is:")
 print(calc_circle(pi, r))
 
-#   delete the key/value pairs in the store
-
+# Removing the key/value pairs using the respective keys
 store.delete("pi")
 store.delete("r")
